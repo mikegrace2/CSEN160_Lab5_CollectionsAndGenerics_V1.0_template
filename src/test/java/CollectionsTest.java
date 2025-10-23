0d15153b46223c561eea9873d4a9af08a422bbf2
Michael Schimpf <mschimpf@scu.edu>
2025-10-23 10:10:07 -0400

    Add JUnit stuff

---
+++ src/test/java/CollectionsTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CollectionsTest {
+    @Test
+    void test1() throws Exception {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Test
+    void test2() throws Exception {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Test
+    void test3() throws Exception {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Test
+    void test4() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Test
+    void test5() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Test
+    void test6() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
